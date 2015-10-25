54165139eefa6f96afd00d3707628af4daf37004
Stormblessed <raleuenberger@gmail.com>
2015-10-24 19:06:04 -0500

    stuff
    more stuff

--- processes/connect.php
+++ processes/connect.php
@@ -3,7 +3,12 @@
 	$db_username="root";
 	$db_password="";
 	$db_name="dailyfantasystats";
-		
+	
+	//$db_host    ="raleuenbergercom.ipagemysql.com";
+	//$db_username="umn_dfsai_admin";
+	//$db_password="WonderfulSandwich!7";
+	//$db_name    ="dfsai";
+	
 	mysql_connect("$db_host", "$db_username", "$db_password")or die("Can’t Connect to the Server");
 	mysql_select_db("$db_name")or die("Can’t Select the Database");
 	
